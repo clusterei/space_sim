ffad6c9df07e2a24b18d960233d8a1f19c143912
clusterei <peterlinus01@outlook.com>
2025-07-26 12:50:31 +0200

    idk smt

--- global.gd
+++ global.gd
@@ -4,10 +4,12 @@ const grav_constant: float = 0.02
 
 func get_gravity_accel(pos: Vector3) -> Vector3:
 	var accel := Vector3.ZERO
-	for object in get_tree().get_nodes_in_group("celestial_objects"):
-		var diff: Vector3 = object.position - pos
+	for c_object in get_tree().get_nodes_in_group("celestial_objects"):
+		var diff: Vector3 = c_object.position - pos
 		var d: float = diff.length()
 		if d == 0.: continue
-		var strength: float = grav_constant * object.mass / pow(d, 2)
+		var subsurface_mod: float = min(d / c_object.surface_radius, 1.)
+		var strength: float = grav_constant * c_object.mass / pow(d, 2) * subsurface_mod
+		#####reduce strength the deeper below surfice one is
 		accel += diff / d * strength
 	return accel
