e69d78e8fa9e9bccb64d23ff46fd5119ddc6a3c2
clusterei <peterlinus01@outlook.com>
2025-07-26 21:27:44 +0200

    visual stuff

--- general/global.gd
+++ general/global.gd
@@ -2,10 +2,11 @@ extends Node
 
 const grav_constant: float = 0.02
 var previous_game_speed: float = Engine.time_scale
+@onready var cel_objects := get_tree().get_nodes_in_group("celestial_objects")####dont have group and make manually or detect group updates automatically
 
 func get_gravity_accel(pos: Vector3) -> Vector3:
 	var accel := Vector3.ZERO
-	for c_object in get_tree().get_nodes_in_group("celestial_objects"):
+	for c_object in cel_objects:
 		var diff: Vector3 = c_object.position - pos
 		var d: float = diff.length()
 		if d == 0.: continue
@@ -30,3 +31,41 @@ func get_first_intersected_celestial_object(pos: Vector3, dir: Vector3) -> celes
 func change_game_speed(speed: float) -> void:
 	Engine.time_scale = speed
 	Engine.physics_ticks_per_second = int(max(speed, 1.) * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
+
+func predict_paths() -> Array[PackedVector3Array]:#####work in progress
+	var num_steps: int = 200
+	var stepsize: float = 0.1
+	#var ref_frame_obj_index: int = 0
+	
+	var num_objs: int = cel_objects.size()
+	var path_arr_arr: Array[PackedVector3Array] = []
+	var pos_arr := PackedVector3Array()
+	var vel_arr := PackedVector3Array()
+	var mass_arr := PackedFloat32Array()
+	for obj in cel_objects:
+		var path_arr := PackedVector3Array()
+		path_arr.resize(num_steps)
+		path_arr_arr.append(path_arr)
+		pos_arr.append(obj.position)
+		vel_arr.append(obj.linear_velocity)
+		mass_arr.append(obj.mass)
+	var new_pos_arr := PackedVector3Array()
+	new_pos_arr.resize(num_objs)
+	
+	for i in num_steps:
+		#var ref_frame_offset := pos_arr[ref_frame_obj_index]#to adjust for ref frame moving
+		for j in num_objs:
+			var accel := Vector3.ZERO
+			var pos: Vector3 = pos_arr[j]
+			path_arr_arr[j][i] = pos#updates path
+			for k in num_objs:
+				var diff: Vector3 = pos_arr[k] - pos
+				var d: float = diff.length()
+				if d == 0.: continue
+				var strength: float = grav_constant * mass_arr[k] / pow(d, 2)
+				accel += diff / d * strength
+			vel_arr[j] += accel * stepsize
+			new_pos_arr[j] = pos + vel_arr[j] * stepsize# - ref_frame_offset
+		pos_arr = new_pos_arr.duplicate()
+	
+	return path_arr_arr
