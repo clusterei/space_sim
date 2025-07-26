a4744075edccc3df9ef02ab94f29db7f5303f90c
clusterei <peterlinus01@outlook.com>
2025-07-26 18:30:10 +0200

    Revert "Merge pull request #14 from clusterei/cam_test"
    This reverts commit 8540a301a5713571da49e48c2bca37babd257ae5, reversing
    changes made to 106383b97e016029420e5b4d6bb55564a1851bd1.

--- global.gd
+++ global.gd
@@ -1,7 +1,6 @@
 extends Node
 
 const grav_constant: float = 0.02
-var previous_game_speed: float = Engine.time_scale
 
 func get_gravity_accel(pos: Vector3) -> Vector3:
 	var accel := Vector3.ZERO
@@ -11,22 +10,6 @@ func get_gravity_accel(pos: Vector3) -> Vector3:
 		if d == 0.: continue
 		var subsurface_mod: float = min(d / c_object.surface_radius, 1.)
 		var strength: float = grav_constant * c_object.mass / pow(d, 2) * subsurface_mod
+		#####reduce strength the deeper below surfice one is
 		accel += diff / d * strength
 	return accel
-
-func get_first_intersected_celestial_object(pos: Vector3, dir: Vector3) -> celestial_object:
-	var hit_obj = null
-	var hit_obj_d: float = INF
-	for cl_object in get_tree().get_nodes_in_group("celestial_objects"):
-		var obj_pos: Vector3 = cl_object.position
-		var closest_point_d: float = (obj_pos - pos).dot(dir)
-		if closest_point_d > hit_obj_d or closest_point_d < 0.: continue
-		var point_obj_d_sq: float = (pos + dir * closest_point_d - obj_pos).length_squared()
-		if pow(cl_object.surface_radius, 2) >= point_obj_d_sq:
-			hit_obj = cl_object
-			hit_obj_d = closest_point_d
-	return hit_obj
-
-func change_game_speed(speed: float) -> void:
-	Engine.time_scale = speed
-	Engine.physics_ticks_per_second = int(max(speed, 1.) * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
